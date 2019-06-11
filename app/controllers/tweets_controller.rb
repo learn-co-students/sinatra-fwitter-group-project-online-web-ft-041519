d078744edb9cc82c83291a94b04bf4e9d21bd85b
Erin Stalzer <erinstalzer@gmail.com>
2019-06-11 14:39:41 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,17 @@
 class TweetsController < ApplicationController
 
+  get '/tweets/new' do
+    erb :new
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.new(content: params["content"])
+    erb :tweets
+  end
+
+  get '/tweets/:id' do
+    @tweet = Tweet.find_by(id: params[:id])
+    erb :show
+  end
 
 end
